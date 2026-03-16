e12e2f359d607be3a92b558182e03d82ac6ca374
EduRuanor <derr9517@gmail.com>
2026-03-16 08:47:39 -0600

    Update Script_Prometeo.sql
    Actualizacion para creacion de SQL. Se añadieron tablas y registros correspondientes para poder utilizar el modulo de estudiantes.

--- Database/Script_Prometeo.sql
+++ Database/Script_Prometeo.sql
@@ -1,4 +1,4 @@
-﻿--  Creación Base de Datos (Grupo 11)
+--  Creación Base de Datos (Grupo 11)
 CREATE DATABASE SistemaPrometeo;
 GO
 
@@ -15,6 +15,7 @@ CREATE TABLE Usuarios (
     Rol NVARCHAR(20) NOT NULL, -- 'Administrador', 'Docente', 'Estudiante'
     
     -- Campos para Estudiantes (pueden ser NULL si el usuario es docente/admin)
+	EstudianteID INT,
     Carnet NVARCHAR(20) NULL,
     Carrera NVARCHAR(100) NULL,
     
@@ -31,11 +32,85 @@ CREATE TABLE Ciclos (
     FechaCierre DATE NOT NULL
 );
 
+CREATE TABLE Carreras (
+    CarreraID INT IDENTITY PRIMARY KEY,
+    Nombre VARCHAR(150)
+);
+
+CREATE TABLE Estudiantes (
+    EstudianteID INT IDENTITY PRIMARY KEY,
+    UsuarioID INT UNIQUE,
+    Nombres VARCHAR(100),
+    Apellidos VARCHAR(100),
+    Carnet VARCHAR(20),
+    CarreraID INT,
+    CicloID INT,
+    FOREIGN KEY (UsuarioID) REFERENCES Usuarios(UsuarioID)
+);
+
+CREATE TABLE AreasInvestigacion (
+    AreaID INT IDENTITY PRIMARY KEY,
+    Nombre VARCHAR(100)
+);
+
+CREATE TABLE EstadosProyecto (
+    EstadoID INT IDENTITY PRIMARY KEY,
+    Nombre VARCHAR(50)
+);
+
+CREATE TABLE Proyectos (
+    ProyectoID INT IDENTITY PRIMARY KEY,
+    EstudianteID INT,
+    NombreProyecto VARCHAR(200),
+    TituloProyecto VARCHAR(300),
+    Descripcion TEXT,
+    Objetivo TEXT,
+    AreaID INT,
+    Tipo CHAR(1),
+    EstadoID INT,
+    FechaEnvio DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (EstudianteID) REFERENCES Estudiantes(EstudianteID),
+    FOREIGN KEY (AreaID) REFERENCES AreasInvestigacion(AreaID),
+    FOREIGN KEY (EstadoID) REFERENCES EstadosProyecto(EstadoID)
+);
+
+CREATE TABLE DocumentosProyecto (
+    DocumentoID INT IDENTITY PRIMARY KEY,
+    ProyectoID INT,
+    NombreArchivo VARCHAR(200),
+    RutaArchivo VARCHAR(300),
+    FechaSubida DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (ProyectoID) REFERENCES Proyectos(ProyectoID)
+);
+
+CREATE TABLE Observaciones (
+    ObservacionID INT IDENTITY PRIMARY KEY,
+    ProyectoID INT,
+    Comentario TEXT,
+    Fecha DATETIME DEFAULT GETDATE(),
+    FOREIGN KEY (ProyectoID) REFERENCES Proyectos(ProyectoID)
+);
+
 -- AGREGAMOS UN ADMIN DE PRUEBA (Para podernos loguear)
-INSERT INTO Usuarios (Nombres, Apellidos, Correo, Contraseña, Rol)
-VALUES ('Admin', 'General', 'admin@mail.utec.edu.sv', '123456', 'Administrador');
+INSERT INTO Estudiantes (UsuarioID,Nombres,Apellidos,Carnet,CarreraID,CicloID)
+VALUES (1,'Paco','Flores','1212341234',1,1)
+
+INSERT INTO Usuarios (Nombres, Apellidos, Correo, Contraseña, Rol, EstudianteID)
+VALUES ('Admin', 'General', 'admin@mail.utec.edu.sv', '123456', 'Administrador',1);
 
 -- DATOS DE PRUEBA PARA CICLOS
 INSERT INTO Ciclos (NombreCiclo, FechaInicio, FechaCierre)
 VALUES ('Ciclo 01-2026', '2026-01-15', '2026-06-15');
 GO
+
+INSERT INTO EstadosProyecto VALUES ('Pendiente')
+INSERT INTO EstadosProyecto VALUES ('En revisión')
+INSERT INTO EstadosProyecto VALUES ('Aprobado')
+INSERT INTO EstadosProyecto VALUES ('Rechazado')
+
+INSERT INTO AreasInvestigacion VALUES ('Inteligencia Artificial')
+INSERT INTO AreasInvestigacion VALUES ('Desarrollo Web')
+INSERT INTO AreasInvestigacion VALUES ('Ciberseguridad')
+
+INSERT INTO Carreras VALUES ('Ingenieria en Sistemas')
+INSERT INTO Carreras VALUES ('Ingenieria en Sistemas Virtual')
